196dab69749a1a39be863862e29d8e1acca64450
thanneman <thanneman1@gmail.com>
2020-01-06 20:32:47 -0700

    added tests

---
+++ test/app.test.js
@@ -0,0 +1,50 @@
+const supertest = require('supertest');
+const app = require('../app');
+const { expect } = require('chai');
+
+describe('GET /apps', () => {
+  it('should return an array of apps', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const theApp = res.body[0];
+        expect(theApp).to.include.all.keys(
+          'App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver'
+        );
+      });
+  });
+
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'invalid' })
+      .expect(400, 'Sort must be one of rating or app')
+  });
+
+  it('should sort by rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'rating' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        let sorted = true;
+
+        let i = 0;
+        while(i < res.body.length -1) {
+          const appAtI = res.body[i];
+          const appAtIPlus1 = res.body[i + 1];
+          if(appAtIPlus1.Rating < appAtI.Rating) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+});
